26f971d85c4b249f4be78fda3a12ea926d45ffb1
Kelll31 <46720761+Kelll31@users.noreply.github.com>
2026-02-11 17:40:50 +0300

    v1.4: Переход на EncButton вместо самописного энкодера

--- encoder.h
+++ encoder.h
@@ -1,146 +1,84 @@
 /*
  * МОДУЛЬ ЭНКОДЕРА
- * Обработка вращения и нажатий
+ * Обработка вращения и нажатий с использованием библиотеки EncButton
+ * Современная замена устаревшей GyverEncoder
  */
 
 #ifndef ENCODER_H
 #define ENCODER_H
 
 #include <Arduino.h>
+#include <EncButton.h>
 #include "config.h"
 
 class EncoderModule {
 private:
-  int8_t position;
-  int8_t lastPosition;
-  bool buttonState;
-  bool lastButtonState;
-  unsigned long buttonPressTime;
-  unsigned long lastRotateTime;
-  bool longPressDetected;
-
-  uint8_t clkState;
-  uint8_t dtState;
-  uint8_t lastClkState;
-
+  EncButton<EB_TICK, ENCODER_DT, ENCODER_CLK, ENCODER_SW> enc;
+  
 public:
-  EncoderModule() : position(0), 
-                    lastPosition(0),
-                    buttonState(HIGH),
-                    lastButtonState(HIGH),
-                    buttonPressTime(0),
-                    lastRotateTime(0),
-                    longPressDetected(false),
-                    clkState(HIGH),
-                    dtState(HIGH),
-                    lastClkState(HIGH) {}
+  EncoderModule() {}
 
   // Инициализация энкодера
   void begin() {
-    pinMode(ENCODER_CLK, INPUT_PULLUP);
-    pinMode(ENCODER_DT, INPUT_PULLUP);
-    pinMode(ENCODER_SW, INPUT_PULLUP);
-
-    lastClkState = digitalRead(ENCODER_CLK);
+    enc.setEncType(EB_STEP4_LOW);  // Тип энкодера (активный низкий сигнал)
   }
 
-  // Обработка энкодера (вызывать в loop)
+  // Опрос энкодера (вызывать в loop)
   void tick() {
-    // Чтение состояния CLK
-    clkState = digitalRead(ENCODER_CLK);
-
-    // Обнаружение вращения
-    if (clkState != lastClkState && clkState == LOW) {
-      dtState = digitalRead(ENCODER_DT);
-
-      // Определение направления
-      if (dtState != clkState) {
-        position++; // Поворот по часовой стрелке
-      } else {
-        position--; // Поворот против часовой стрелки
-      }
-
-      lastRotateTime = millis();
-    }
-
-    lastClkState = clkState;
-
-    // Обработка кнопки
-    bool currentButtonState = digitalRead(ENCODER_SW);
-
-    if (currentButtonState != lastButtonState) {
-      delay(10); // Дебаунсинг
-      currentButtonState = digitalRead(ENCODER_SW);
-
-      if (currentButtonState == LOW && lastButtonState == HIGH) {
-        // Кнопка нажата
-        buttonPressTime = millis();
-        longPressDetected = false;
-      }
-    }
-
-    // Обнаружение длинного нажатия
-    if (currentButtonState == LOW && !longPressDetected) {
-      if (millis() - buttonPressTime >= LONG_PRESS_TIME) {
-        longPressDetected = true;
-      }
-    }
-
-    buttonState = currentButtonState;
-    lastButtonState = currentButtonState;
+    enc.tick();
   }
 
-  // Проверка изменения позиции
-  bool isChanged() {
-    if (position != lastPosition) {
-      lastPosition = position;
-      return true;
-    }
-    return false;
-  }
-
-  // Получение изменения позиции
+  // Получение изменения позиции (-1, 0, +1)
   int8_t getDelta() {
-    int8_t delta = position - lastPosition;
-    lastPosition = position;
-    return delta;
-  }
-
-  // Получение текущей позиции
-  int8_t getPosition() const {
-    return position;
-  }
-
-  // Сброс позиции
-  void resetPosition() {
-    position = 0;
-    lastPosition = 0;
+    if (enc.turn()) {
+      return enc.dir();
+    }
+    return 0;
   }
 
   // Проверка короткого нажатия
   bool isClick() {
-    if (buttonState == HIGH && lastButtonState == LOW) {
-      unsigned long pressDuration = millis() - buttonPressTime;
-      if (pressDuration < LONG_PRESS_TIME && !longPressDetected) {
-        return true;
-      }
-    }
-    return false;
+    return enc.click();
+  }
+
+  // Проверка двойного клика
+  bool isDouble() {
+    return enc.hasClicks(2);
   }
 
   // Проверка длинного нажатия
   bool isLongPress() {
-    return longPressDetected;
+    return enc.hold();
+  }
+
+  // Очистка флага длинного нажатия (не требуется с EncButton)
+  void clearLongPress() {
+    // EncButton автоматически сбрасывает флаги
   }
 
   // Проверка быстрого вращения
-  bool isFastRotate() const {
-    return (millis() - lastRotateTime) < ENCODER_FAST_THRESHOLD;
+  bool isFastRotate() {
+    return enc.fast();
   }
 
-  // Очистка флага длинного нажатия
-  void clearLongPress() {
-    longPressDetected = false;
+  // Проверка нажатого вращения
+  bool isTurnH() {
+    return enc.turnH();
+  }
+
+  // Сброс позиции (не применимо к EncButton)
+  void resetPosition() {
+    // EncButton не хранит абсолютную позицию
+  }
+
+  // Получение нажатия кнопки (состояние)
+  bool isPressed() {
+    return enc.pressing();
+  }
+
+  // Проверка одиночного клика
+  bool isSingle() {
+    return enc.click();
   }
 };
 
