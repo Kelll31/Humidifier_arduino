33c1bbb12dcc0e7868643b43377e4e892159b0f9
Kelll31 <46720761+Kelll31@users.noreply.github.com>
2026-02-12 00:34:58 +0300

    v1.7.3: Убрана проверка датчика воды при старте, добавлена проверка WATER_SENSOR_ENABLED

--- Humidifier_arduino.ino
+++ Humidifier_arduino.ino
@@ -1,12 +1,15 @@
 /*
- * АВТОМАТИЧЕСКИЙ УВЛАЖНИТЕЛЬ v1.7.2
+ * АВТОМАТИЧЕСКИЙ УВЛАЖНИТЕЛЬ v1.7.3
+ * 
+ * v1.7.3:
+ * - Добавлена возможность отключения датчика воды
+ * - Исправлена проблема с перезагрузкой при старте
  * 
  * v1.7.2:
- * - 2 режима главного экрана: ДАННЫЕ и ГРАФИК
- * - Переключение вращением энкодера
+ * - 2 режима главного экрана
  * 
  * v1.7:
- * - Датчик уровня воды (A0)
+ * - Датчик уровня воды
  * - Детектор открытого окна
  * - Расширенная статистика
  * - Адаптивное обучение
@@ -38,17 +41,16 @@ unsigned long lastLearningUpdate = 0;
 bool dataUpdated = false;
 
 void setup() {
+  // Отключаем watchdog на время инициализации
+  wdt_disable();
+  
   storage.begin();
   
   display.begin();
   display.showSplash();
-  delay(2000);
+  delay(1500);
   
   analytics.begin();
-  
-  if (!analytics.isWaterSensorPresent()) {
-    display.showWaterSensorError();
-  }
   
   sensor.begin();
   sensor.setStorage(&storage);
@@ -56,6 +58,8 @@ void setup() {
   humidifier.begin();
   humidifier.setStorage(&storage);
   menu.begin(&display, &encoder, &storage, &sensor, &humidifier);
+  
+  // Включаем watchdog после инициализации
   wdt_enable(WDTO_4S);
   
   lastEncoderActivity = millis();
@@ -90,12 +94,24 @@ void loop() {
       float hum = sensor.getHumidity();
       bool running = humidifier.isRunning();
 
-      bool waterOK = analytics.checkWaterLevel();
-      analytics.updateWindowDetector(temp);
-      bool windowOpen = analytics.isWindowOpen();
+      #if WATER_SENSOR_ENABLED
+        bool waterOK = analytics.checkWaterLevel();
+      #else
+        bool waterOK = true;
+      #endif
+      
+      #if WINDOW_DETECTOR_ENABLED
+        analytics.updateWindowDetector(temp);
+        bool windowOpen = analytics.isWindowOpen();
+      #else
+        bool windowOpen = false;
+      #endif
 
       display.addGraphPoint(hum, running);
-      analytics.addSample(temp, hum, running);
+      
+      #if STATS_ENABLED
+        analytics.addSample(temp, hum, running);
+      #endif
 
       if (waterOK && !windowOpen) {
         humidifier.control(hum, storage.getMinHumidity(), storage.getMaxHumidity(), sensor.isOK());
@@ -114,22 +130,35 @@ void loop() {
     }
   }
   
-  if (millis() - lastLearningUpdate >= 3600000UL) {
-    lastLearningUpdate = millis();
-    analytics.updateLearning();
-  }
+  #if LEARNING_ENABLED && STATS_ENABLED
+    if (millis() - lastLearningUpdate >= 3600000UL) {
+      lastLearningUpdate = millis();
+      analytics.updateLearning();
+    }
+  #endif
 
   if (menu.isActive()) {
     menu.tick();
     menu.draw();
   } else {
-    // На главном экране - вращение переключает режим
     if (encoder.isRight() || encoder.isLeft()) {
       display.toggleMode();
       dataUpdated = true;
     }
     
     if (dataUpdated) {
+      #if WATER_SENSOR_ENABLED
+        bool waterLow = analytics.isWaterLow();
+      #else
+        bool waterLow = false;
+      #endif
+      
+      #if WINDOW_DETECTOR_ENABLED
+        bool windowOpen = analytics.isWindowOpen();
+      #else
+        bool windowOpen = false;
+      #endif
+      
       display.drawMainScreen(
         sensor.getTemperature(),
         sensor.getHumidity(),
@@ -137,8 +166,8 @@ void loop() {
         humidifier.isRunning(),
         storage.getWorkTime(),
         sensor.isOK(),
-        analytics.isWaterLow(),
-        analytics.isWindowOpen()
+        waterLow,
+        windowOpen
       );
       dataUpdated = false;
     }
@@ -151,7 +180,9 @@ void loop() {
 
   if (millis() - lastSaveTime >= AUTOSAVE_INTERVAL) {
     lastSaveTime = millis();
-    analytics.saveHourlyStats();
+    #if STATS_ENABLED
+      analytics.saveHourlyStats();
+    #endif
     storage.saveDirect();
   }
 
